58fd86831bce420ec2856d78ead9e4235304e3fe
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-06-04 16:38:45 +0530

    perform arithamtic operation in nodeJs [vid-8]

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const add=require("./math");
+// const add=require("./math");
+// const mul=require("./math");
+
 // console.log(global);
 
 // global.console.log("subscribe")
@@ -9,5 +11,10 @@ const add=require("./math");
 // globalThis.console.log(module)
 
 
+const math=require("./math")
+
 //common js
-console.log(add(5,10));
+console.log(math.add(5,10));
+console.log(math.sub(10,5));
+console.log(math.mul(5,10));
+console.log(math.div(10,5));
