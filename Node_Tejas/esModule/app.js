5e05866f2f14d681ddadfd6992ad8d36c2deb7b5
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-06-08 15:12:29 +0530

    export and import in nodeJs [vid-19]

---
+++ Node_Tejas/esModule/app.js
@@ -0,0 +1,30 @@
+// const add=require("./math");
+// const mul=require("./math");
+
+// console.log(global);
+
+// global.console.log("subscribe")
+// globalThis.console.log("Thapa Technical")
+
+
+
+// globalThis.console.log(module)
+
+
+
+
+// const math=require("./math")
+
+
+// import multiply from "./math.js";
+// import {mul,div} from "./math.js";  
+import {mul,div,sub,add} from "./math.js";
+// import * as math from "./math.js";
+
+//common js
+console.log(mul(60,10));
+console.log(div(10,5));
+console.log(sub(10,5));
+console.log(add(5,10));
+// console.log(multiply(60,10));
+
