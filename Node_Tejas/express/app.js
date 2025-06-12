108413c4cbb6083f824fef8dec2da2c735d4ab08
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-06-12 14:17:39 +0530

    Environment Variables in Node.js & Express | Setup .env Files  [vid-38]

--- Node_Tejas/express/app.js
+++ Node_Tejas/express/app.js
@@ -32,7 +32,7 @@ app.get("/contact", (req, res) => {
 })
 
 
-    const PORT = 3000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running at PORT : ${PORT}`);
         console.log();
