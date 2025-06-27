2d2041b3ce7932924999177a22e72d29717053ef
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-06-27 12:52:13 +0530

    Bcrypt vs Argon2  [vid-77]

--- Node_Tejas/controllers/auth.controller.js
+++ Node_Tejas/controllers/auth.controller.js
@@ -4,6 +4,12 @@ export const getRegisterPage = (req, res) => {
     return es.render("../views/auth/register");
 };
 
+export const postRegister=async(req,res)=>{
+    console.log(req.body);
+    res.redirect("/login")
+    
+}
+
 export const getLoginPage = (req, res) => {
     return es.render("../views/auth/register");
 };
