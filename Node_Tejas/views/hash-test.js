6f3e24d450c539d20b2b1055a6ca4f4f15baa43e
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-06-28 23:52:48 +0530

    twt working

--- Node_Tejas/views/hash-test.js
+++ Node_Tejas/views/hash-test.js
@@ -1,48 +1,26 @@
-// // import argon2 from "argon2";
-// import bcrypt from "bcryptjs";
-
-// async function hashPassword(password){
-//     return bcrypt.hash(password,12)
-// }
-
-// async function verifyPassword(password,hashPassword) {
-//     return bcrypt.compare(password,hashPassword);
-// }
-
-// const password1=Array.from({length:71}).fill("x").join("");
-
-// const password2=Array.from({length:71}).fill("x").join("")+Math.random();
-
-// console.log(password1,password2);
-
-// const hashPassword1=await hashPassword(password1);
-// const hashPassword2=await hashPassword(password2);
-
-// console.log("1-1",await verifyPassword(password1,hashPassword));
-// console.log("2-1",await verifyPassword(password2,hashPassword1));
-// console.log("2-2",await verifyPassword(password2,hashPassword2));
-// console.log("1-2",await verifyPassword(password1,hashPassword2));
-
-
+// import argon2 from "argon2";
 import bcrypt from "bcryptjs";
 
-async function hashPassword(password) {
-    return bcrypt.hash(password, 12);
+async function hashPassword(password){
+    return bcrypt.hash(password,12)
 }
 
-async function verifyPassword(password, hashedPassword) {
-    return bcrypt.compare(password, hashedPassword);
+async function verifyPassword(password,hashPassword) {
+    return bcrypt.compare(password,hashPassword);
 }
 
-const password1 = Array.from({ length: 71 }).fill("x").join("");
-const password2 = Array.from({ length: 71 }).fill("x").join("") + Math.random();
+const password1=Array.from({length:71}).fill("x").join("");
 
-console.log(password1, password2);
+const password2=Array.from({length:71}).fill("x").join("")+Math.random();
 
-const hashPassword1 = await hashPassword(password1);
-const hashPassword2 = await hashPassword(password2);
+console.log(password1,password2);
 
-console.log("1-1", await verifyPassword(password1, hashPassword1));
-console.log("2-1", await verifyPassword(password2, hashPassword1));
-console.log("2-2", await verifyPassword(password2, hashPassword2));
-console.log("1-2", await verifyPassword(password1, hashPassword2));
+const hashPassword1=await hashPassword(password1);
+const hashPassword2=await hashPassword(password2);
+
+console.log("1-1",await verifyPassword(password1,hashPassword));
+console.log("2-1",await verifyPassword(password2,hashPassword1));
+console.log("2-2",await verifyPassword(password2,hashPassword2));
+console.log("1-2",await verifyPassword(password1,hashPassword2));
+
+
